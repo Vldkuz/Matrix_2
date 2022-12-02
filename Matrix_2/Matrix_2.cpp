a92c15e916fca51b021434bb9dacd45e5b126a95
Vladislav <kuznetsov230304@gmail.com>
2022-12-02 22:01:54 +0500

    Fix

---
+++ Matrix_2/Matrix_2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    bool right = 1;
+
+    double** matrix = new double* [N];
+    for (int i = 0; i < N; ++i)
+        matrix[i] = new double[N];
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < N; ++i)//столбец
+    {
+        for (int j = 0; j < i; ++j) //cтрок
+        { 
+            for (int k = i + 1; k < N; ++k) //ниже диагонали
+            {
+                if (matrix[j][i]<matrix[k][i])
+                {
+                    right = 0;
+                    cout << right << endl;
+                    return 0;
+                }
+            }
+        }
+    }
+    
+
+    cout << right;
+    return 0;
+}
+
